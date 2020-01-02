991364877bd864a9a57695f8447110c37e3d05d0
ntruonghau <ntruonghau>
2020-01-02 20:09:16 +0700

    2-dec-2019

--- app_school/app_hoc_sinh.py
+++
@@ -1,7 +0,0 @@
-from flask import Markup, request, render_template, url_for, session, redirect
-from app_school import app
-
-
-@app.route('/hoc-sinh', methods=['GET','POST'])
-def hoc_sinh():
-    return render_template('hoc_sinh/hs_dashboard.html')
