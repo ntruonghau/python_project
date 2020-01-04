890848895143e50810bbd3064577f5fa3e533a42
ntruonghau <ntruonghau>
2020-01-04 19:18:11 +0700

    4-jan-2020

---
+++ app_school/xu_ly/bang_diem/XL_Bang_diem.py
@@ -0,0 +1,17 @@
+from app_school import app, db_session
+from app_school.xu_ly.Xu_ly_Model import BangDiem, HocSinh
+
+def doc_danh_sach_bang_diem_hoc(): # select field tupple choice
+    ds_bang_diem = []
+    try:
+        ds_bangdiem = db_session.query(BangDiem).all()
+        for bang_diem in ds_bangdiem:
+            bd = (bang_diem.IDbang_diem, bang_diem.Tenbang_diem)
+            ds_bang_diem.append(bd)
+    except:
+        pass
+    return ds_bang_diem
+
+def tao_bang_diem_cho_hoc_sinh(id_hoc_sinh):
+    hoc_sinh = db_session.query(HocSinh).filter(HocSinh.IDHocSinh == id_hoc_sinh).one()
+
