e9b4a248ef05363e3e9fb79ed1b441e34ee43654
KruLb01 <55050513+KruLb01@users.noreply.github.com>
2020-01-06 18:44:50 +0700

    -

--- app_school/app_trang_chu.py
+++ app_school/app_trang_chu.py
@@ -61,9 +61,9 @@ def trang_tra_cuu():
     if request.form.get("Th_Chuoi_Tra_cuu") :
         Chuoi_Tra_cuu = request.form.get("Th_Chuoi_Tra_cuu")
         dia_chi_mh = "/trang-chu/tra-cuu/" + Chuoi_Tra_cuu
-        if int(Chuoi_Tra_cuu) > int(dem) :
+        if int(Chuoi_Tra_cuu) >= int(dem) :
             dia_chi_mh = "/trang-chu/tra-cuu/0"
-    return render_template('trang_chu/tra-cuu-hoc-sinh.html' , dia_chi_mh=dia_chi_mh)
+    return render_template('trang_chu/tra-cuu-hoc-sinh.html' , dia_chi_mh=dia_chi_mh , Chuoi_Tra_cuu=Chuoi_Tra_cuu)
 
 @app.route('/trang-chu/tra-cuu/<string:Chuoi_Tra_cuu>/', methods=['GET','POST'])
 def trang_tra_cuu_theo_id(Chuoi_Tra_cuu):
